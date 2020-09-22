378429c8f61fa4701e3a61df8ccaa2e474976ef4
Caio Fabrini <caiofabrini@outlook.com>
2020-09-21 22:17:07 -0300

    criei a register e implementei algumas classes

---
+++ MarvelApp/MarvelApp/Register/RegisterVC.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterVC.swift
+//  MarvelApp
+//
+//  Created by Caio on 21/09/20.
+//
+
+import UIKit
+
+class RegisterVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+
+}
