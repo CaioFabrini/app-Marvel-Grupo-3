8a024e3e4a3f6d1a562c230b81e7f5e71c8c02f8
Caio Fabrini <caiofabrini@outlook.com>
2020-09-27 11:24:19 -0300

    criei a classe CharacterDetailVC e implementei os metodos da TableView

---
+++ MarvelApp/MarvelApp/Detail/Character Detail/CharacterDetailVC.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterDetailVC.swift
+//  MarvelApp
+//
+//  Created by Caio on 27/09/20.
+//
+
+import Foundation
+import UIKit
+
+class CharacterDetailVC:BaseViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    
+    
+}
+
+
+extension CharacterDetailVC: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
