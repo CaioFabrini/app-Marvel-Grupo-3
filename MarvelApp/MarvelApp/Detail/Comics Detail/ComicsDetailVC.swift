1f10e8f466fea86457cdd360ed704e50745e89b2
Caio Fabrini <caiofabrini@outlook.com>
2020-10-03 17:46:36 -0300

    criei ComicsDetailVC

---
+++ MarvelApp/MarvelApp/Detail/Comics Detail/ComicsDetailVC.swift
@@ -0,0 +1,27 @@
+//
+//  ComicsDetailVC.swift
+//  MarvelApp
+//
+//  Created by Caio on 03/10/20.
+//
+
+import Foundation
+import UIKit
+
+class ComicsDetailVC:BaseViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
