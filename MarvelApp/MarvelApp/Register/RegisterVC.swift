ecd7d4d92d7fab000987e6e88dc388f55a49d8aa
Guilherme Martins de Andrade <guihhandrade@hotmail.com>
2020-10-09 19:57:36 -0300

    Update Constraints

--- MarvelApp/MarvelApp/Register/RegisterVC.swift
+++ MarvelApp/MarvelApp/Register/RegisterVC.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class RegisterVC: BaseViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.configScreen()
        
     }
     
+    func configScreen(){
+        self.signUpButton.layer.cornerRadius = 24
+        self.signUpButton.layer.borderWidth = 3
+        self.signUpButton.layer.borderColor = UIColor.white.cgColor
 
 
-
+}
 }
