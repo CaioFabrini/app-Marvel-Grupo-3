9f46fe5c025c9744028b1950be631e9c04b9c4e3
Guilherme Martins de Andrade <guihhandrade@hotmail.com>
2020-10-09 20:36:38 -0300

    Bordas Ok para iphone 11 Pro Max

--- MarvelApp/MarvelApp/Register/RegisterVC.swift
+++ MarvelApp/MarvelApp/Register/RegisterVC.swift
@@ -23,13 +23,32 @@ class RegisterVC: BaseViewController {
 
         self.configScreen()
        
+        
+        
     }
+    
     
     func configScreen(){
         self.signUpButton.layer.cornerRadius = 24
         self.signUpButton.layer.borderWidth = 3
         self.signUpButton.layer.borderColor = UIColor.white.cgColor
 
+        self.usernameTextField.layer.cornerRadius = 24
+        self.usernameTextField.clipsToBounds = true
+        self.emailTextField.layer.cornerRadius = 24
+        self.usernameTextField.layer.masksToBounds = true
+        self.emailTextField.layer.masksToBounds = true
+        self.cpfTextField.layer.masksToBounds = true
+        self.cpfTextField.layer.cornerRadius = 24
+        self.passwordTextField.layer.cornerRadius = 24
+        self.passwordTextField.layer.masksToBounds = true
+        self.confirmPasswordTextField.layer.masksToBounds = true
+        self.confirmPasswordTextField.layer.cornerRadius = 24
+        self.usernameTextField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        self.emailTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        self.cpfTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        self.passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        self.confirmPasswordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
 
 }
 }
