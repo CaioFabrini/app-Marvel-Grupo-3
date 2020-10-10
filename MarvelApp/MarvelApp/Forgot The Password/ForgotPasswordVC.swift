fcb073f7b9e53c3ed4359d898e460b004839f93a
Amanda Nolasco <nolasco.amanda@outlook.com>
2020-10-10 00:31:47 -0300

    Criei a tela View Controller do Forgot Password

---
+++ MarvelApp/MarvelApp/Forgot The Password/ForgotPasswordVC.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotPasswordVC.swift
+//  MarvelApp
+//
+//  Created by Amanda Nolasco on 09/10/20.
+//
+
+import UIKit
+
+class ForgotPasswordVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+}
