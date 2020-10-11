6ba4d5832d16681c7005cae6ed4104281be00fda
Amanda Nolasco <nolasco.amanda@outlook.com>
2020-10-11 19:44:28 -0300

    Criei as IBOutlets da tela e actions, arredondei os textfields e botões, coloquei a borda do botão update.

--- MarvelApp/MarvelApp/Forgot The Password/ForgotPasswordVC.swift
+++ MarvelApp/MarvelApp/Forgot The Password/ForgotPasswordVC.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ForgotPasswordVC: BaseViewController {
-
+    //commit: Criei as IBOutlets da tela e actions
+    
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configScreen()
        
     }
+    // commit: arredondei os textfields e botões
+    func configScreen(){
+        
+        self.updateButton.layer.cornerRadius = 25
+        self.updateButton.layer.borderWidth = 3
+        self.updateButton.layer.borderColor = UIColor.white.cgColor
+        self.cpfTextField.layer.cornerRadius = 30
+        self.passwordTextField.layer.cornerRadius = 30
+        self.confirmPasswordTextField.layer.cornerRadius = 30
+        self.cpfTextField.layer.masksToBounds = true
+        self.passwordTextField.layer.masksToBounds = true
+        self.confirmPasswordTextField.layer.masksToBounds = true
+    
+    }
+    
+    @IBAction func tappedUpdateButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        
+    }
+    
+    }
+    
+    
+    
 
-}
