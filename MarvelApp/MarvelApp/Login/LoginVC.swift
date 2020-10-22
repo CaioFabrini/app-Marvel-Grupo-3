1b5b0ce09945328e084ef5527ff108f950242025
Guilherme Martins de Andrade <guihhandrade@hotmail.com>
2020-10-22 20:33:57 -0300

    Adicionei extension hideKeyboardWhenTappedAround

--- MarvelApp/MarvelApp/Login/LoginVC.swift
+++ MarvelApp/MarvelApp/Login/LoginVC.swift
@@ -18,6 +18,7 @@ class LoginVC: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.hideKeyboardWhenTappedAround()
         self.configScreen()
         
     }
