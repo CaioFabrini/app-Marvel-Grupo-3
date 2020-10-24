f829f2a3ef9f1ac38a1ddecfdeef629945460c29
Gilmar Jr <gilmar_mendonca@live.com>
2020-10-24 17:16:25 -0300

    Navegação da view Settings corrigida.

--- MarvelApp/MarvelApp/Profile/Config Profile/ConfigProfileVC.swift
+++ MarvelApp/MarvelApp/Profile/Config Profile/ConfigProfileVC.swift
@@ -7,29 +7,23 @@
 
 import UIKit
 
-class ConfigProfileVC: UIViewController {
+class ConfigProfileVC: BaseViewController {
 
-    @IBAction func backButton(_ sender: UIButton) {
-//        let storyBoard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
-//        let profileVC = storyBoard.instantiateViewController(withIdentifier: "ProfileVC2")
-//        self.present(profileVC, animated: true, completion: nil)
-        self.dismiss(animated: true, completion: nil)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    */
-
+    
+    @IBAction func AccountSettingButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "AccountSettingsSegue" , sender: nil)
+        
+    }
+    
 }
