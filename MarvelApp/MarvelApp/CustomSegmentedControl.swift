32b5ea477d10f07a56acf0655b3ae269688d2cc5
Amanda Nolasco <nolasco.amanda@outlook.com>
2020-10-25 03:16:53 -0300

    Alterações no código do segmented control

--- MarvelApp/MarvelApp/CustomSegmentedControl.swift
+++ MarvelApp/MarvelApp/CustomSegmentedControl.swift
@@ -10,7 +10,9 @@ import UIKit
 @IBDesignable
 
 class CustomSegmentedControl: UIView {
-
+    
+ var items = [UIButton]()
+    
 @IBInspectable
     var borderWidth: CGFloat = 0{
         didSet {
@@ -23,10 +25,46 @@ class CustomSegmentedControl: UIView {
         didSet {
             layer.borderColor = borderColor.cgColor
         }
+}
+    
+    @IBInspectable
+    var segmentedItems: String = "" {
+        didSet{
+            updateView()
+        }
     
 }
     
+    func updateView() {
+        items.removeAll()
+        subviews.forEach{ $0.removeFromSuperView() }
+        
+        let itemTitles = segmentedItems.components(separatedBy: ",")
+        for itemTitle in itemTitles {
+            let item =  UIButton(type: .system)
+            item.setTitle(itemTitle, for: .normal)
+            items.append(item)
+            
+        }
+        
+        let sv = UIStackView(arrangedSubviews: items)
+        sv.axis = .horizontal
+        sv.alignment = .fill
+        sv.distribution = .fillProportionally
+        addSubview(sv)
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        sv.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        sv.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        sv.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        
+        
+        
+    }
+    
     override func draw(_ rect: CGRect) {
+        
+        layer.cornerRadius = frame.height/2
         
     }
     
