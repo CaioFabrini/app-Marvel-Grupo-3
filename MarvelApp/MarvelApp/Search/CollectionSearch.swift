4b4882d4a55abb49548cef3acf46742ceadda686
Erick Sens <ericksens10@gmail.com>
2020-10-31 13:32:46 -0300

    Finalizando Search

--- MarvelApp/MarvelApp/Search/CollectionSearch.swift
+++ MarvelApp/MarvelApp/Search/CollectionSearch.swift
@@ -7,57 +7,33 @@
 
 import UIKit
 
-class CollectionSearch : UIViewController, UICollectionViewDelegate {
+class CollectionSearch : UIViewController {
     
-    var arraySearchName : [Personagem] = []
+    var dataSource : [Personagem] = []
     var name : String?
-    
-    @IBOutlet weak var CollectionViewSearch: UICollectionView!
-    
+
+    @IBOutlet weak var collectionViewSearch: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.arraySearchName  = [Personagem(nome: "TESTE", descricao: "", bibiografia: "", idade: "", image: "Imagem 24.png",type:"character"),
-                               Personagem(nome: "Teste2", descricao: "", bibiografia: "", idade: "", image: "Imagem 25.png", type: "comic"),
-                               Personagem(nome: "Teste23", descricao: "", bibiografia: "", idade: "", image: "Imagem 36.png", type: "comic"),
-                               Personagem(nome: "Teste4", descricao: "", bibiografia: "", idade: "", image: "Imagem 26.png", type: "comic"),
-                               Personagem(nome: "Teste5", descricao: "", bibiografia: "", idade: "", image: "Imagem 27.png", type: "comic"),
-                               Personagem(nome: "Teste6", descricao: "", bibiografia: "", idade: "", image: "Imagem 28.png", type: "comic"),
-                               Personagem(nome: "Teste7", descricao: "", bibiografia: "", idade: "", image: "Imagem 29.png", type: "comic"),
-                               Personagem(nome: "Teste8", descricao: "", bibiografia: "", idade: "", image: "Imagem 30.png", type: "comic"),
-                               Personagem(nome: "Teste9", descricao: "", bibiografia: "", idade: "", image: "Imagem 31.png", type: "comic"),
-                               Personagem(nome: "Teste10", descricao: "", bibiografia: "", idade: "", image: "Imagem 38.png", type: "comic"),
-                               Personagem(nome: "Teste11", descricao: "", bibiografia: "", idade: "", image: "Imagem 39.png", type: "comic"),
-                               Personagem(nome: "Teste12", descricao: "", bibiografia: "", idade: "", image: "Imagem 40.png", type: "comic"),
-                               Personagem(nome: "Teste13", descricao: "", bibiografia: "", idade: "", image: "Imagem 43.png", type: "comic"),
-                               Personagem(nome: "Teste14", descricao: "", bibiografia: "", idade: "", image: "Imagem 45.png", type: "comic"),
-                               Personagem(nome: "Teste15", descricao: "", bibiografia: "", idade: "", image: "Imagem 46.png", type: "comic"),
-                               Personagem(nome: "Teste16", descricao: "", bibiografia: "", idade: "", image: "Imagem 47.png", type: "comic"),
-                               Personagem(nome: "Teste17", descricao: "", bibiografia: "", idade: "", image: "Imagem 51.png", type: "comic"),
-                               Personagem(nome: "Teste18", descricao: "", bibiografia: "", idade: "", image: "Imagem 52.png", type: "comic"),
-                               Personagem(nome: "Teste19", descricao: "", bibiografia: "", idade: "", image: "Imagem 53.png", type: "comic"),
-                               Personagem(nome: "Teste20", descricao: "", bibiografia: "", idade: "", image: "Imagem 54.png", type: "comic"),
-                               Personagem(nome: "Teste21", descricao: "", bibiografia: "", idade: "", image: "Imagem 55.png", type: "comic")
-                               ]
-        
-        
-        self.CollectionViewSearch.register(UINib(nibName: "CharactersCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CharactersCollectionViewCell")
+
+        self.collectionViewSearch.register(UINib(nibName: "CharactersCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CharactersCollectionViewCell")
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 170, height: 200)
-        self.CollectionViewSearch.collectionViewLayout = layout
-        self.CollectionViewSearch.delegate = self
-        self.CollectionViewSearch.dataSource = self
-        self.CollectionViewSearch.reloadData()
+        self.collectionViewSearch.collectionViewLayout = layout
+        self.collectionViewSearch.reloadData()
+
     }
 }
 
-extension CollectionSearch: UICollisionBehaviorDelegate , UICollectionViewDataSource{
+extension CollectionSearch: UICollectionViewDelegate , UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return arraySearchName.count
+        return dataSource.count
 
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: CharactersCollectionViewCell? = collectionView.dequeueReusableCell(withReuseIdentifier: "CharactersCollectionViewCell", for: indexPath) as? CharactersCollectionViewCell
-        cell?.setupCell(Personagem: self.arraySearchName[indexPath.item])
+        cell?.setupCell(Personagem: self.dataSource[indexPath.item])
         return cell ?? UICollectionViewCell()
     }
 }
--- MarvelApp/MarvelApp/Search/Search.swift
+++ MarvelApp/MarvelApp/Search/Search.swift
@@ -7,63 +7,60 @@
 
 import UIKit
 
-class Search :UIViewController {
+class Search: UIViewController {
     
-    @IBOutlet weak var SearchBar: UISearchBar!
-    
-    
-    var arraySearch = [String]()
-    var searching = false
-    
-   
+    @IBOutlet weak var searchBar: UISearchBar!
+    var arrayFiltered = [Personagem]()
+    var arrayDataSource = [Personagem]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        func prepare( for segue: UIStoryboardSegue, sender: AnyObject?){
-           SearchBar.resignFirstResponder()
-           let  vc = segue.destination as! CollectionSearch
-           vc.name = SearchBar.text
-   }
-                
+            
+        self.arrayDataSource  = [Personagem(nome: "TESTE", descricao: "", bibiografia: "", idade: "", image: "Imagem 24.png",type:"character"),
+                               Personagem(nome: "Teste2", descricao: "", bibiografia: "", idade: "", image: "Imagem 25.png", type: "comic"),
+                               Personagem(nome: "Teste23", descricao: "", bibiografia: "", idade: "", image: "Imagem 36.png", type: "comic"),
+                               Personagem(nome: "Teste4", descricao: "", bibiografia: "", idade: "", image: "Imagem 26.png", type: "comic"),
+                               Personagem(nome: "Teste5", descricao: "", bibiografia: "", idade: "", image: "Imagem 27.png", type: "comic"),
+                               Personagem(nome: "Teste6", descricao: "", bibiografia: "", idade: "", image: "Imagem 28.png", type: "comic"),
+                               Personagem(nome: "Teste7", descricao: "", bibiografia: "", idade: "", image: "Imagem 29.png", type: "comic"),
+                               Personagem(nome: "Teste8", descricao: "", bibiografia: "", idade: "", image: "Imagem 30.png", type: "comic"),
+                               Personagem(nome: "Teste9", descricao: "", bibiografia: "", idade: "", image: "Imagem 31.png", type: "comic"),
+                               Personagem(nome: "Teste10", descricao: "", bibiografia: "", idade: "", image: "Imagem 38.png", type: "comic"),
+                               Personagem(nome: "Teste11", descricao: "", bibiografia: "", idade: "", image: "Imagem 39.png", type: "comic"),
+                               Personagem(nome: "Teste12", descricao: "", bibiografia: "", idade: "", image: "Imagem 40.png", type: "comic"),
+                               Personagem(nome: "Teste13", descricao: "", bibiografia: "", idade: "", image: "Imagem 43.png", type: "comic"),
+                               Personagem(nome: "Teste14", descricao: "", bibiografia: "", idade: "", image: "Imagem 45.png", type: "comic"),
+                               Personagem(nome: "Teste15", descricao: "", bibiografia: "", idade: "", image: "Imagem 46.png", type: "comic"),
+                               Personagem(nome: "Teste16", descricao: "", bibiografia: "", idade: "", image: "Imagem 47.png", type: "comic"),
+                               Personagem(nome: "Teste17", descricao: "", bibiografia: "", idade: "", image: "Imagem 51.png", type: "comic"),
+                               Personagem(nome: "Teste18", descricao: "", bibiografia: "", idade: "", image: "Imagem 52.png", type: "comic"),
+                               Personagem(nome: "Teste19", descricao: "", bibiografia: "", idade: "", image: "Imagem 53.png", type: "comic"),
+                               Personagem(nome: "Teste20", descricao: "", bibiografia: "", idade: "", image: "Imagem 54.png", type: "comic"),
+                               Personagem(nome: "Teste21", descricao: "", bibiografia: "", idade: "", image: "Imagem 55.png", type: "comic")
+                               ]
+
     }
     
     @IBAction func CharacterButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "result", sender: nil)
+        print("testando")
+        self.performSegue(withIdentifier: "resultView", sender: nil)
+        
     }
-    @IBAction func ComicButton(_ sender: Any) {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "resultView" {
+            if let resultViewController = segue.destination as? CollectionSearch {
+                resultViewController.dataSource = arrayFiltered
+            }
+        }
     }
+
 }
 
-//extension Search: UICollectionViewDelegate , UICollectionViewDataSource{
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        if searching{
-//             return arraySearch.count
-//
-//        }else{
-//            return arraySearchName.count
-//        }
-//
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell: CharactersCollectionViewCell? = collectionView.dequeueReusableCell(withReuseIdentifier: "CharactersCollectionViewCell", for: indexPath) as? CharactersCollectionViewCell
-//        if searching{
-//            cell?.LblNome?.text = arraySearch[indexPath.row]
-//        }
-//
-//        else {
-//            cell?.setupCell(Personagem:self.arraySearchName[indexPath.item])
-//
-//        }
-//        return cell!
-//
-//    }
-//}
-//extension Search: UISearchBarDelegate{
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//    //    arraySearch = arraySearchName.filter({$0.nome == searchText.lowercased()})
-//        searching = true
-//    //    SearchCollectionView.reloadData()
-//    }
-//}
+extension Search: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        arrayFiltered = arrayDataSource.filter({$0.nome.lowercased().contains(searchText.lowercased())})
+        
+    }
+    
+}
 
